959c1df2b450e8a9ed3d694b45769d27435e8b4a
mehradman <m.radmanmehr98@gmail.com>
2025-02-03 10:18:48 +0330

    Update blue-green.sh

--- blue-green.sh
+++ blue-green.sh
@@ -8,7 +8,7 @@ sed -i 's|PORT=.*|PORT=$PORT|' /home/deployer/blue/.env
 docker compose -f /home/deployer/blue/docker-compose.yml down && docker compose -f /home/deployer/blue/docker-compose.yml up -d
 sed -i 's|localhost:8082 weight=0|localhost:8082 weight=1|' /home/deployer/nginx.conf
 nginx -t
-systemctl reload nginx.service
+sudo /etc/init.d/nginx reload
 
 sed -i 's|IMAGE_REPO=.*|IMAGE_REPO=$IMAGE_REPO|' /home/deployer/green/.env
 sed -i 's|IMAGE_NAME=.*|IMAGE_NAME=$IMAGE_NAME|' /home/deployer/green/.env
@@ -17,14 +17,14 @@ sed -i 's|PORT=.*|PORT=$PORT|' /home/deployer/green/.env
 
 sed -i 's|localhost:8083 weight=1|localhost:8083 weight=0|' /home/deployer/nginx.conf
 nginx -t
-systemctl reload nginx.service
+sudo /etc/init.d/nginx reload
 
 docker compose -f /home/deployer/green/docker-compose.yml down && docker compose -f /home/deployer/green/docker-compose.yml up -d
 sed -i 's|localhost:8083 weight=0|localhost:8083 weight=1|' /home/deployer/nginx.conf
 nginx -t
-systemctl reload nginx.service
+sudo /etc/init.d/nginx reload
 
 sed -i 's|localhost:8082 weight=1|localhost:8082 weight=0|' /home/deployer/nginx.conf
 nginx -t
-systemctl reload nginx.service
+sudo /etc/init.d/nginx reload
 docker compose -f /home/deployer/blue/docker-compose.yml down
