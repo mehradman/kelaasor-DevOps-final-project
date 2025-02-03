4147a162e84495804b827acf15be5edd05bf9335
mehradman <m.radmanmehr98@gmail.com>
2025-02-03 12:52:23 +0330

    Update blue-green.sh

--- blue-green.sh
+++ blue-green.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-IMAGE_REPO=$1
-IMAGE_NAME=$2
-IMAGE_TAG=$3
 BLUE_PORT=8082
 GREEN_PORT=8083
 
 sed -i 's|IMAGE_REPO=.*|IMAGE_REPO=$IMAGE_REPO|' /home/deployer/blue/.env
 sed -i 's|IMAGE_NAME=.*|IMAGE_NAME=$IMAGE_NAME|' /home/deployer/blue/.env
 sed -i 's|IMAGE_TAG=.*|IMAGE_TAG=$IMAGE_TAG|' /home/deployer/blue/.env
-sed -i 's|PORT=.*|PORT=$PORT|' /home/deployer/blue/.env
+sed -i 's|PORT=.*|PORT=$BLUE_PORT|' /home/deployer/blue/.env
 
 docker compose -f /home/deployer/blue/docker-compose.yml down && docker compose -f /home/deployer/blue/docker-compose.yml up -d
 sed -i 's|localhost:8082 weight=0|localhost:8082 weight=1|' /home/deployer/nginx.conf
@@ -19,7 +16,7 @@ sudo /etc/init.d/nginx reload
 sed -i 's|IMAGE_REPO=.*|IMAGE_REPO=$IMAGE_REPO|' /home/deployer/green/.env
 sed -i 's|IMAGE_NAME=.*|IMAGE_NAME=$IMAGE_NAME|' /home/deployer/green/.env
 sed -i 's|IMAGE_TAG=.*|IMAGE_TAG=$IMAGE_TAG|' /home/deployer/green/.env
-sed -i 's|PORT=.*|PORT=$PORT|' /home/deployer/green/.env
+sed -i 's|PORT=.*|PORT=$GREEN_PORT|' /home/deployer/green/.env
 
 sed -i 's|localhost:8083 weight=1|localhost:8083 weight=0|' /home/deployer/nginx.conf
 nginx -t
